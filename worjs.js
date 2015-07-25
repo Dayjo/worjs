127d729a74fda80dde322e8282e3ddf1bbe6d500
Joel <dayjoaspen+github@gmail.com>
2015-07-25 21:00:02 +0100

    attempt webkitspeech recognition

--- worjs.js
+++ worjs.js
@@ -71,3 +71,12 @@ document.getElementById('find-words-form').addEventListener('submit', function(e
 	e.preventDefault();
 	return false;
 });
+
+
+var q = document.getElementById('letters');
+var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event) {
+  if (event.results.length > 0) {
+    q.value = event.results[0][0].transcript;
+  }
+}
