67876909bb32394faec61922ad754c08073efab0
Joel Day <Dayjo@Joels-MBP.home>
2016-04-24 23:43:52 +0100

    loader, lots of improvements

--- worjs.js
+++ worjs.js
@@ -4,27 +4,65 @@ var words = [];
 var letters = "";
 var locks = {};
 var lockreg = '';
+var runTimeout;
+var ignoreWords = JSON.parse(localStorage.getItem('ignoreWords')) || [];
+
+var scores = {
+	a: 1,
+	e: 1,
+	i: 1,
+	o: 1,
+	u: 1,
+	b:3, c:3, d:2, f:4,
+	g: 2, h: 4, j:8, k: 5,
+	l:1, m:3, n:1, p:3,
+	q: 10, r:1, s:1, t: 1,
+	v:4, w:4, x: 8, y:4, z: 10
+};
 
 function find_words() {
     var reg = new RegExp("^(?!.*?(.).*?\1)["+letters+"]*["+letters+"]*$");
   	found = [];
     f = [];
-    for ( var i in words ) {
-      w = words[i];
-  
-      contains = w.match(reg);
-  	
-      if ( w.length && contains ) {
-        f.push(w);
-      }
-    }
- 
+
+
+	for ( var i in words ) {
+	      w = words[i];
+
+		  if ( ignoreWords.indexOf(w) > -1 ) {
+			  continue;
+		  }
+
+	      contains = true;
+	      if ( letters.length >= w.length ) {
+	        tempL = letters;
+
+	        for ( var l = 0; l < w.length; l++ ){
+
+	          if ( tempL.indexOf(w.charAt(l)) === -1 ) {
+	             if ( tempL.indexOf('*') === -1 ) {
+	                contains = false;
+	             }
+	            else {
+	                tempL = tempL.replace('*', '');
+	            }
+	          }
+	          else {
+	            tempL = tempL.replace(w.charAt(l), '');
+	          }
+	        }
+
+	        if ( w.length && contains ) {
+	          f.push(w);
+	        }
+	      }
+	    }
 
   for ( var i in f ) {
   	var add = true;
 
-	add = (f[i].match(lockreg).length > 0);
-	
+	add = f[i].match(lockreg);
+
   	if ( add && f[i].length ) {
 		for ( var j in letters ) {
 			var num_letters = letters.split(letters[j]).length -1;
@@ -43,47 +81,57 @@ function find_words() {
 		found.push(f[i]);
 	}
   }
-  
-  found.sort();
+
   found.sort(function(a, b){
-  	if ( b.length == a.length ) {
-  		return 0;
-  	}
     return (b.length - a.length || a.localeCompare(b) ); // ASC -> a - b; DESC -> b - a
+  });
+
+  found.sort(function(a,b){
+	  return score(b) - score(a);
   });
 
   var html = '';
   for ( i = 0; i < found.length; i++ ) {
-  	html += '<li>';
+  	html += '<li data-word="'+found[i]+'">';
   	for ( var j = 0; j<found[i].length; j++ ){
 		html += '<a data-i="' + j + '">' + found[i][j] + '</a>';
   	}
+	html += '<span class="score">' + score(found[i]) + '</span>';
+	html += '<span class="delete">&times;</span>';
   	html += '</li>';
-  	
+
   }
 
   document.getElementById('results-list').innerHTML = '<ul>' + html + '</ul>';
+	setTimeout(function(){ Loader.stop();},200);
+}
 
+function score(w){
+	var score = 0;
+	for( var i = 0; i < w.length; i++ ) {
+		score += scores[w.charAt(i)];
+	}
+	return score;
 }
 
 function update_locks() {
-	
+
 	lockreg = '';
 	var ll = 0;
 	for ( var l in locks ) {
-	  
+
 	  if ( l - ll > 1 ) {
 	    for( var i = 1; i < (l-ll); i++ ){
 	      lockreg += '[a-z]';
 	    }
 	  }
-	  
+
 	  lockreg += locks[l];
 	  ll = l;
 	}
 	console.log(locks);
 	console.log(lockreg);
-	
+
 	find_words();
 }
 
@@ -101,29 +149,51 @@ client.onreadystatechange = function() {
 
 client.send();
 
+function run(e) {
+	clearTimeout(runTimeout);
+	Loader.start('Finding Words...', '#results-list');
+	runTimeout = setTimeout(function(){
 
-document.getElementById('find-words-form').addEventListener('submit', function(e){
 
-	letters = document.getElementById('letters').value.toLowerCase();
-	find_words();
+		var l = document.getElementById('letters').value.toLowerCase();
+		l = l.replace(/[^a-z\*]/g,"");
+
+		if ( letters != l ) {
+			letters = l;
+			locks = {}; lockreg = '';
+			find_words();
+		}
+	}, 300);
 
 	e.preventDefault();
 	return false;
-});
+
+}
+
+document.getElementById('find-words-form').addEventListener('submit', run);
+document.getElementById('letters').addEventListener('keyup', run);
 
 document.getElementById('results-list').addEventListener('click', function(event){
 	if ( event.target.tagName == 'A' ) {
 		var i = event.target.getAttribute('data-i');
 		var char = event.target.textContent;
-		
+
 		if ( locks[i] == char ) {
 			delete locks[i];
 		} else {
-			locks[i] = char;	
+			locks[i] = char;
 		}
-		
+
 		update_locks();
-	}	
+	}
+	else if ( event.target.classList.contains('delete') ) {
+		var word = event.target.parentNode.getAttribute('data-word');
+		if ( confirm("Are you sure you want to remove '"+word+"' it will be ignored!") ) {
+			ignoreWords.push(event.target.parentNode.getAttribute('data-word'));
+			localStorage.setItem('ignoreWords', JSON.stringify(ignoreWords));
+			event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+		}
+	}
 });
 
 var q = document.getElementById('letters');
